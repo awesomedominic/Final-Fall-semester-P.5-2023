ad32e2b070012c228f2be5c93fd38d525f12c7dc
awesomedominic <dominicrodriguez845@riversideunified.org>
2023-12-13 13:32:32 -0800

    Working on Milestone 2

---
+++ Mini Game/Assets/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player collected a coin.");
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
